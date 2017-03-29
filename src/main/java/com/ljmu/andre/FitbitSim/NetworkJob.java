c25c7c5bd7fd3335baa1aa6d7f7dd42fc24e635b
Andre Marques <andrerm124@gmail.com>
2017-03-29 22:56:26 +0100

    Added unstaged files

--- src/main/java/com/ljmu/andre/FitbitSim/NetworkJob.java
+++ src/main/java/com/ljmu/andre/FitbitSim/NetworkJob.java
@@ -5,7 +5,11 @@ import hu.mta.sztaki.lpds.cloud.simulator.helpers.job.Job;
 /**
  * Created by Andre on 29/03/2017.
  */
-public class RealJob extends Job {
+public class NetworkJob extends Job {
+    private long packetSize;
+    private String source;
+    private String target;
+
     /**
      * The generic constructor to be used by most of the trace generators and in
      * most of the use cases.
@@ -13,24 +17,29 @@ public class RealJob extends Job {
      * Please note the id of the job is automatically generated with this
      * constructor.
      *
-     * @param id         the id of the job, if it is null, then the id will be
-     *                   internally assigned.
-     * @param submit     the time instance (in secs) the job is submitted
-     * @param queue      the duration (in secs) it took for the job to get to
-     *                   execution.
-     * @param exec       the execution duration of the job on its original resource.
-     * @param nprocs     number of processors used by the particular job during its
-     *                   execution
-     * @param ppCpu
-     * @param ppMem
-     * @param user       the user who submitted the job
+     * @param id        the id of the job, if it is null, then the id will be
+     *                  internally assigned.
+     * @param submit    the time instance (in secs) the job is submitted
+     * @param user      the user who submitted the job
      * @param group
-     * @param executable
      * @param preceding
-     * @param delayAfter
      */
-    public RealJob(String id, long submit, long queue, long exec, int nprocs, double ppCpu, long ppMem, String user, String group, String executable, Job preceding, long delayAfter) {
-        super(id, submit, queue, exec, nprocs, ppCpu, ppMem, user, group, executable, preceding, delayAfter);
+    public NetworkJob(String id, String source, String target, long submit, long packetSize, String user, String group, Job preceding) {
+        super(id, submit, 0, 0, 0, 0, 0, user, group, null, preceding, 0);
+        this.packetSize = packetSize;
+    }
+
+
+    public long getPacketSize() {
+        return packetSize;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTarget() {
+        return target;
     }
 
     @Override public void started() {
--- src/main/java/com/ljmu/andre/FitbitSim/Packets/DataPacket.java
+++ src/main/java/com/ljmu/andre/FitbitSim/Packets/DataPacket.java
@@ -3,5 +3,8 @@ package com.ljmu.andre.FitbitSim.Packets;
 /**
  * Created by Andre on 29/03/2017.
  */
-public class DataPacket {
+public class DataPacket extends BasePacket {
+    public DataPacket(String myid, long mysize, boolean vary) {
+        super(myid, mysize, vary);
+    }
 }
