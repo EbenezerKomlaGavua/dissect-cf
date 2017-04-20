ddda1c053d3c265a2e966825cc77d858aeb2d23f
Andre Marques <andrerm124@gmail.com>
2017-04-20 23:16:15 +0100

    Added a logger to the station

--- src/main/java/com/ljmu/andre/WeatherSimulation/Station.java
+++ src/main/java/com/ljmu/andre/WeatherSimulation/Station.java
@@ -1,6 +1,7 @@
 package com.ljmu.andre.WeatherSimulation;
 
 import com.ljmu.andre.SimulationHelpers.Device;
+import com.ljmu.andre.SimulationHelpers.Utils.Logger;
 
 import hu.mta.sztaki.lpds.cloud.simulator.DeferredEvent;
 import hu.mta.sztaki.lpds.cloud.simulator.helpers.trace.GenericTraceProducer;
@@ -9,6 +10,7 @@ import hu.mta.sztaki.lpds.cloud.simulator.helpers.trace.GenericTraceProducer;
  * Created by Andre on 13/04/2017.
  */
 public class Station extends Device {
+    private static final Logger logger = new Logger(Station.class);
     int currentJob = 0;
     int sensors;
 
@@ -32,6 +34,7 @@ public class Station extends Device {
 
         for(int i = 0; i < sensors; i++) {
             new Metered(1000);
+            logger.log("Meter");
         }
     }
 
