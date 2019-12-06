2ad2bfbcaf0419a6f37f570a6f5bbbcdce33f529
DZIDZOR <gavua@iit.uni-miskolc.hu>
2019-12-06 02:39:13 +0100

    auxilliary commits

---
+++ src/main/java/com/ljmu/andre/SimulationHelpers/SimClient.java
@@ -0,0 +1,25 @@
+package com.ljmu.andre.SimulationHelpers;
+
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.resourcemodel.ResourceConsumption;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.resourcemodel.ResourceConsumption.ConsumptionEvent;
+//import hu.unimiskolc.iit.distsys.ExercisesBase;
+
+public class SimClient implements ConsumptionEvent{
+	
+	
+	
+	
+
+	@Override
+	public void conComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void conCancelled(ResourceConsumption problematic) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
